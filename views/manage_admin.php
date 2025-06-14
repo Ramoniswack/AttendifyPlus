ee89d7458444d05baca3a634127300aea718de4e
SubashGrg <131990703+SubashGrg@users.noreply.github.com>
2025-06-15 01:01:55 +0545

    chore: handled attendance module

--- views/manage_admin.php
+++ views/manage_admin.php
@@ -1,4 +1,44 @@
 <?php
+session_start();
+if (!isset($_SESSION['UserID']) || strtolower($_SESSION['Role']) !== 'admin') {
+  header("Location: login.php");
+  exit();
+}
+include '../config/db_config.php';
 
-echo " here "
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Manage Teachers | Attendify+</title>
+  <link rel="stylesheet" href="../assets/css/manage_teacher.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
+  <script src="../assets/js/lucide.min.js"></script>
+  <script src="../assets/js/manage_teacher.js" defer></script>
+</head>
+
+<body>
+  <?php include 'sidebar_admin_dashboard.php'; ?>
+
+  <!-- Navbar -->
+  <?php include 'navbar_admin.php'; ?>
+  <?php include 'navbar_admin.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manage admin</title>
+</head>
+<body>
+    
+
+</body>
+</html>
